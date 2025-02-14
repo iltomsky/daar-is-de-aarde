58747e032df32a90b8e81ca951c6d3d7d43966ec
iltomsky <nathan.arnouts2012@gmail.com>
2025-02-14 15:26:37 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-input.onGesture(Gesture.Shake, function () {
-	
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showArrow(ArrowNames.North)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showArrow(ArrowNames.East)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showArrow(ArrowNames.South)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
 })
